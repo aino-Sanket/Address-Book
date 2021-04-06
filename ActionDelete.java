838851bace68959eacca9dc2bb225915b99fc973
aino-Sanket <81403258+aino-Sanket@users.noreply.github.com>
2021-04-06 22:34:47 +0530

    Update ActionDelete.java

--- ActionDelete.java
+++ ActionDelete.java
@@ -1,11 +1,31 @@
+/**
+ * 
+ * @author Sanket Kolhe
+ *
+ *<h1> This Class Deletes a specific Record Present in the Address-Book </h1>
+ *
+ */
 
-public class ActionDelete implements Action{
+
+public class ActionDelete extends PersonAddressInfo  implements Action{
 	
 
-	public Object execute()
+	public Object execute()				//Overridden execute method of Action Interface
 	{
-		System.out.println("Inside Delete");
 		
-		return null;
+		
+		ActionFind af=new ActionFind();			//Create an instance of ActionFind Class to check wether record is present or not in the Address-Book
+		int key=(Integer)af.execute();			//Call the execute method of the ActionFind Class which returns the key of record if found
+		
+		if(key!=-1)
+		{
+			addressEntries.remove(key);		//Delete required record to be deleted from Address-Book.	
+			
+			System.out.println("Record Deleted SuccessFully......\n");
+			
+		}
+		
+		
+		return Integer.valueOf(1);
 	}
 }
