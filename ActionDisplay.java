f881e6b9b72c8ed20194ad3658fa9e191c6424df
aino-Sanket <81403258+aino-Sanket@users.noreply.github.com>
2021-04-06 22:35:27 +0530

    Update ActionDisplay.java

--- ActionDisplay.java
+++ ActionDisplay.java
@@ -1,51 +1,47 @@
+/**
+ *  <h1> This class Displays all the records in Address-Book</h1>
+ * 
+ */
+
 import java.util.*;
 
 public class ActionDisplay extends ActionCreate  implements Action{
 
-	public Object execute()
+	public Object execute()		//overridden execute method of Action Interface
 	{
 		
-	/*	for(PersonAddressInfo eachAddress: addressEntries)
-		{
-			System.out.println("Name is: " + eachAddress.name);
-			
-			System.out.println("Street name  is: " + eachAddress.street);
-			
-			System.out.println("City name is: " + eachAddress.city);
-			
-			System.out.println("State name is : " + eachAddress.state);
-			
-			System.out.println("Country name is: " + eachAddress.country);
-			
-			System.out.println("Zipcode  is: " + eachAddress.zipcode);
-			
-			
-		}
-		*/
+		boolean flag=false;
 		
-		 Iterator hmIterator = addressEntries.entrySet().iterator();
-		 while (hmIterator.hasNext()) {
-	            Map.Entry mapElement = (Map.Entry)hmIterator.next();
-	            ActionCreate eachAddress=(ActionCreate)mapElement.getValue();
+		 for(Map.Entry<Integer,ActionCreate> eachAddress : addressEntries.entrySet())	//Display all the records from the address-book
+			{
 				
-	            System.out.println("Name is: " + eachAddress.name);
+		
+					
+			 	System.out.println("\nName is: " + eachAddress.getValue().name);
 				
-				System.out.println("Street name  is: " + eachAddress.street);
+				System.out.println("Street name  is: " + eachAddress.getValue().street);
 				
-				System.out.println("City name is: " + eachAddress.city);
+				System.out.println("City name is: " + eachAddress.getValue().city);
 				
-				System.out.println("State name is : " + eachAddress.state);
+				System.out.println("State name is : " + eachAddress.getValue().state);
 				
-				System.out.println("Country name is: " + eachAddress.country);
+				System.out.println("Country name is: " + eachAddress.getValue().country);
 				
-				System.out.println("Zipcode  is: " + eachAddress.zipcode);
+				System.out.println("Zipcode  is: " + eachAddress.getValue().zipcode + "\n");
 				
-		 }
-		System.out.println("Inside Display");
+				flag=true;
+		 
+			}
+		 
+		 	if(flag==false)
+		 	{
+		 		System.out.println("No Elements yet in Address-Book to be displayed \n");
+		 	}
+		 	
 		
 		
 		
-		return Integer.valueOf(1);
+		 return Integer.valueOf(1);
 	}
 
 }
