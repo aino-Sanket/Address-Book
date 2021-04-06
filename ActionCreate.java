497593c25b7d1d07c64ce40e7fee942ca10b51c6
aino-Sanket <81403258+aino-Sanket@users.noreply.github.com>
2021-04-06 22:33:43 +0530

    Update ActionCreate.java

--- ActionCreate.java
+++ ActionCreate.java
@@ -4,8 +4,8 @@
  *
  *<h1>This Class is used to create the new address for a person</h1>
  *
- *This class accepts the requied parameters to create the Address and creates the address for the person
- *and stores it into a arraylist one at a time.
+ *This class accepts the required parameters to create the Address and creates the address for the person
+ *and stores it into a hashmap one at a time.
  */
 
 
@@ -14,14 +14,17 @@ import java.util.*;
 public class ActionCreate  extends PersonAddressInfo   implements Action
 {
 	Scanner sc=new Scanner(System.in);
+	static int i=1;
 	
-	public Object execute()
-	{   int i=1;
+	public Object execute()		//Overridden execute method of the Action Interface
+	{  
+		
+	
 		ActionCreate createObj=new ActionCreate();	//this object accepts the address info
-	
+		
 		
 		System.out.println("Enter the name	");
-		createObj.name=sc.nextLine();				//accept name
+		createObj.name=sc.nextLine();				//accept  name
 		
 		
 		System.out.println("Enter the street	");
@@ -41,16 +44,17 @@ public class ActionCreate  extends PersonAddressInfo   implements Action
 		
 		System.out.println("Enter the zipcode	");
 		createObj.zipcode=sc.nextInt();				//accept zipcode
+		
+		sc.nextLine();               //This line flushes the Scanner before the next Input from the user
 	
 		
 		
 		addressEntries.put(i++,createObj);		//Adds one persons address to the arraylist at a time.
 		
-		System.out.println(" Address Created SuccessFully.... ");
-		
-		
-		
+		System.out.println(" Address Created SuccessFully.... \n");
 		
 		return Integer.valueOf(1);
+		
+		
 	}
 }
